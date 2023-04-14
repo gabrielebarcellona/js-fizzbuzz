251388664c29930689b19385f2df7f2c6d30ebd8
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-04-14 16:59:36 +0200

    last commit

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const elecontainer = document.querySelector('.container')
 
 
-for (let i = 0 ; i <= 100 ; i++) {
-console.log(i);
-elecontainer.innerHTML = elecontainer.innerHTML + ` <div class="quadrato">${i}</div> `
+for (let i = 1 ; i <= 100 ; i++) {
+    console.log(i);
 
-if(i% 3 == 0){
-elecontainer.innerHTML = elecontainer.innerHTML +  ` <div class="quadrato-rosso">${i}</div> `
+     if(i% 15 == 0){
+        elecontainer.innerHTML = elecontainer.innerHTML +  ` <div class="quadrato-rosso">fizzbuzz</div> `
+    }
+
+
+     else if(i% 3 == 0){
+         elecontainer.innerHTML = elecontainer.innerHTML +  ` <div class="quadrato-verde">fizz</div> `
+        }
+
+    else if(i% 5 == 0){
+        elecontainer.innerHTML = elecontainer.innerHTML +  ' <div class="quadrato-giallo">buzz</div> '
+    }
+    
+
+    else {
+        elecontainer.innerHTML = elecontainer.innerHTML +  ' <div class="quadrato">' + i + '</div> '
+    }
 }
 
-if(i% 5 == 0){
-    elecontainer.innerHTML = elecontainer.innerHTML +  ` <div class="quadrato-giallo">${i}</div> `
-}
 
-}
+
+
